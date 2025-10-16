c31b84ff1dada70f5ec8be0d677c6c62bcb46b78
Marc Bärtschi <marc@dolansoft.org>
2025-10-16 20:39:56 +0200

    Add error status handling and decoding for projector lamp modes

--- System.go
+++ System.go
@@ -69,9 +69,96 @@ func (conn *ViewSonic) GetQuickPowerOff() (bool, error) {
 }
 
 // Error status
-// GetErrorStatus returns the raw error status bytes. See PDF Note 3 for decoding.
-func (conn *ViewSonic) GetErrorStatus() ([]byte, error) {
-	return conn.ReadNBytes(0x0D0D) // PDF #177
+type LampModeStatus uint8
+
+const (
+	LampModeStatusStandby                    LampModeStatus = 0x00
+	LampModeStatusIgnition                   LampModeStatus = 0x01 // 0x01, 0x02, 0x03
+	LampModeStatusLampRunUp                  LampModeStatus = 0x04
+	LampModeStatusCoolDown                   LampModeStatus = 0x05
+	LampModeStatusNormalLampOperation        LampModeStatus = 0x06
+	LampModeStatusShutdownUnrecoverableError LampModeStatus = 0x08
+	LampModeStatusPreHeatingPhase            LampModeStatus = 0x09 // 0x09, 0x0C
+)
+
+type LampModeErrorStatus uint16
+
+const (
+	LampModeErrorStatusNoError                         LampModeErrorStatus = 0x00
+	LampModeErrorStatusTemperatureShutdown             LampModeErrorStatus = 0x01
+	LampModeErrorStatusShortCircuit                    LampModeErrorStatus = 0x02
+	LampModeErrorStatusEndOfLampLife                   LampModeErrorStatus = 0x03
+	LampModeErrorStatusLampDidNotIgnite                LampModeErrorStatus = 0x04
+	LampModeErrorStatusLampExtinguishedDuringOperation LampModeErrorStatus = 0x05
+	LampModeErrorStatusLampExtinguishedDuringRunUp     LampModeErrorStatus = 0x06
+	LampModeErrorStatusEEPROMWriteError                LampModeErrorStatus = 0x07
+	LampModeErrorStatusEEPROMWriteBufferOverflow       LampModeErrorStatus = 0x08
+	LampModeErrorStatusUARTBufferOverflow              LampModeErrorStatus = 0x09
+	LampModeErrorStatusLampCurrentCalculationError     LampModeErrorStatus = 0x0A
+	LampModeErrorStatusCorruptedSoftwareConfiguration  LampModeErrorStatus = 0x0B
+	LampModeErrorStatusLampVoltageTooLow               LampModeErrorStatus = 0x0C
+	LampModeErrorStatusEEPROMConfigMismatch            LampModeErrorStatus = 0x0F
+	LampModeErrorStatusMaxPreHeatingTimeElapsed        LampModeErrorStatus = 0x10
+)
+
+type ErrorStatus struct {
+	LampFailCount               uint8
+	LampLitErrorCount           uint8
+	Fan1ErrorCount              uint8
+	Fan2ErrorCount              uint8
+	Fan3ErrorCount              uint8
+	Fan4ErrorCount              uint8
+	Diode1OpenErrorCount        uint8
+	Diode2OpenErrorCount        uint8
+	Diode1ShortErrorCount       uint8
+	Diode2ShortErrorCount       uint8
+	TemperatureErrorCount       uint8
+	Temperature2ErrorCount      uint8
+	FanIC1ErrorCount            uint8
+	ColorWheelErrorCount        uint8
+	ColorWheelStartupErrorCount uint8
+	UART1ErrorCount             uint8
+	AbnormalPowerdown           uint8
+	FirstBurnInErrorMinute      uint32
+	LampStatus                  LampModeStatus
+	LampErrorStatus             LampModeErrorStatus
+}
+
+// GetErrorStatus returns the decoded error status.
+func (conn *ViewSonic) GetErrorStatus() (*ErrorStatus, error) {
+	data, err := conn.ReadNBytes(0x0C0D) // PDF #177
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < 24 {
+		return nil, fmt.Errorf("not enough data for error status, expected 24 bytes, got %d", len(data))
+	}
+
+	status := &ErrorStatus{
+		LampFailCount:               data[0],
+		LampLitErrorCount:           data[1],
+		Fan1ErrorCount:              data[2],
+		Fan2ErrorCount:              data[3],
+		Fan3ErrorCount:              data[4],
+		Fan4ErrorCount:              data[5],
+		Diode1OpenErrorCount:        data[6],
+		Diode2OpenErrorCount:        data[7],
+		Diode1ShortErrorCount:       data[8],
+		Diode2ShortErrorCount:       data[9],
+		TemperatureErrorCount:       data[10],
+		Temperature2ErrorCount:      data[11],
+		FanIC1ErrorCount:            data[12],
+		ColorWheelErrorCount:        data[13],
+		ColorWheelStartupErrorCount: data[14],
+		UART1ErrorCount:             data[15],
+		AbnormalPowerdown:           data[16],
+		FirstBurnInErrorMinute:      binary.LittleEndian.Uint32(data[17:21]),
+		LampStatus:                  LampModeStatus(data[21]),
+		LampErrorStatus:             LampModeErrorStatus(binary.LittleEndian.Uint16(data[22:24])),
+	}
+
+	return status, nil
 }
 
 // Temperature status
